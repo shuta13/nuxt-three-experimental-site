45886324c7fa9dc86fa04e3b8460cdff90e018d8
shuta13 <hollystarsun@gmail.com>
2019-06-01 23:23:22 +0900

    added sample

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -1,35 +1,17 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        nuxt-three-sample
-      </h1>
-      <h2 class="subtitle">
-        nuxt &amp; three sample for practice
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      <a-line />
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import ALine from '~/components/ALine.vue'
 
 export default {
   components: {
-    Logo
+    ALine
   }
 }
 </script>
