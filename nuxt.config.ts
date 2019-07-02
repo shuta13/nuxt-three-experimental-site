62e1db6827d3c5184e506738f6f47b3361f6db6b
shuta13 <hollystarsun@gmail.com>
2019-07-02 19:16:29 +0900

    removed type

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,94 @@
-// import pkg from './package'
 import NuxtConfiguration from '@nuxt/config'
 
-const config: NuxtConfiguration = {
+enum pkg {
+  name = 'nuxt-three-experimental-site',
+  description = 'nuxt &amp; three sample for practice'
+}
 
+const config: NuxtConfiguration = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=DM+Sans&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Ubuntu&display=swap' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    loaders: [{
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      use: 'file-loader'
+    }],
+    extend(config, { isDev, isClient }) {
+      if (config.module === undefined) return;
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        use: 'file-loader',
+        exclude: /(node_modules)/
+      })
+      config.module.rules.push({
+        test: /\.glsl$/,
+        use: {
+          loader: 'webpack-glsl-loader'
+        },
+        exclude: /(node_modules)/
+      })
+      // Run ESLint on save
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
 }
 
 export default config
 
+// legacy code
 // export default {
 //   mode: 'universal',
 
