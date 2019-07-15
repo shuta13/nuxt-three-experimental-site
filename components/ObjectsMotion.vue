61f7f7c2672eebaf3b7de10572a20f09d4097467
shuta13 <hollystarsun@gmail.com>
2019-07-15 20:12:16 +0900

    updated objects motion

--- components/ObjectsMotion.vue
+++ components/ObjectsMotion.vue
@@ -10,12 +10,16 @@ export default {
     return {
       scene: null,
       camera: null,
-      spotLight: null,
-      lightAngle: 0,
       renderer: null,
+      spotLight: null,
       origin: null,
+      started: false,
+      lightAngle: 0,
+      payload: 0,
       stdMtr: null,
-      started: false
+      mshBox: null,
+      mshSphere: null,
+      mshKnot: null
     }
   },
   mounted() {
@@ -53,38 +57,32 @@ export default {
       this.camera.lookAt(this.origin)
     },
     // 処理
-    createFloor() {
-      const geoFloor = new THREE.BoxBufferGeometry(2000, 0.1, 2000)
-      const mtrFloor = new THREE.MeshStandardMaterial({ color: 0x808080, roughness: 0, metalness: 0 })
-      const mshFloor = new THREE.Math(geoFloor, mtrFloor)
-      this.scene.add(mshFloor)
-    },
     createObjects() {
       this.stdMtr = new THREE.MeshStandardMaterial({ color: 'rgb(100, 10, 10)', roughness: 0, metalness: 0 })
 
       const geoBox = new THREE.BoxBufferGeometry(Math.PI, Math.sqrt(2), Math.E)
-      const mshBox = new THREE.Mesh(geoBox, this.stdMtr)
-      mshBox.position.set(0, 0, 0)
-      mshBox.rotation.set(0, Math.PI / 2.0, 0)
-      mshBox.rotation.y = 4
-      mshBox.rotation.z = 20
-      mshBox.castShadow = true
-      mshBox.receiveShadow = true
-      this.scene.add(mshBox)
+      this.mshBox = new THREE.Mesh(geoBox, this.stdMtr)
+      this.mshBox.position.set(0, 0, 0)
+      this.mshBox.rotation.set(0, Math.PI / 2.0, 0)
+      this.mshBox.rotation.y = 4
+      this.mshBox.rotation.z = 20
+      this.mshBox.castShadow = true
+      this.mshBox.receiveShadow = true
+      this.scene.add(this.mshBox)
 
       const geoSphere = new THREE.SphereBufferGeometry(1.5, 32, 32)
-      const mshSphere = new THREE.Mesh(geoSphere, this.stdMtr)
-      mshSphere.position.set(-5, 0, 0)
-      mshSphere.castShadow = true
-      mshSphere.receiveShadow = true
-      this.scene.add(mshSphere)
+      this.mshSphere = new THREE.Mesh(geoSphere, this.stdMtr)
+      this.mshSphere.position.set(-5, 0, 0)
+      this.mshSphere.castShadow = true
+      this.mshSphere.receiveShadow = true
+      this.scene.add(this.mshSphere)
 
       const geoKnot = new THREE.TorusKnotBufferGeometry(1.5, 0.5, 100, 16)
-      const mshKnot = new THREE.Mesh(geoKnot, this.stdMtr)
-      mshKnot.position.set(5, 0, 0)
-      mshKnot.castShadow = true
-      mshKnot.receiveShadow = true
-      this.scene.add(mshKnot)
+      this.mshKnot = new THREE.Mesh(geoKnot, this.stdMtr)
+      this.mshKnot.position.set(5, 0, 0)
+      this.mshKnot.castShadow = true
+      this.mshKnot.receiveShadow = true
+      this.scene.add(this.mshKnot)
     },
     bindWindowEvents() {
       window.addEventListener('resize', this.handleWindowResize, false)
@@ -109,6 +107,11 @@ export default {
       this.lightAngle -= 0.04
       this.spotLight.position.x = 8 * Math.sin(this.lightAngle)
       this.spotLight.position.y = 8 * Math.cos(this.lightAngle)
+
+      this.payload -= 0.03
+      this.mshSphere.position.y = 0.4 * Math.sin(this.payload)
+      if (this.payload < -0.06) this.mshBox.position.y = 0.01 * Math.sin(this.payload)
+      if (this.payload < -0.08) this.mshKnot.position.y = 0.24 * Math.sin(this.payload)
     }
   }
 }
