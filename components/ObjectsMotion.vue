5e1f381d4749fb89214d4947721e15592f24c915
shuta13 <hollystarsun@gmail.com>
2019-07-16 00:19:29 +0900

    gived up

--- components/ObjectsMotion.vue
+++ components/ObjectsMotion.vue
@@ -4,6 +4,7 @@
 
 <script>
 import * as THREE from 'three'
+import { EffectComposer, RenderPass, GlitchPass, MaskPass, ShaderPass, CopyShader, DigitalGlitch, BloomPass, UnrealBloomPass } from 'three-full' // eslint-disable-line
 
 export default {
   data() {
@@ -13,13 +14,22 @@ export default {
       renderer: null,
       spotLight: null,
       origin: null,
+      obj: null,
       started: false,
       lightAngle: 0,
       payload: 0,
+      objectX: -30,
+      objectY: -20,
       stdMtr: null,
       mshBox: null,
       mshSphere: null,
-      mshKnot: null
+      mshKnot: null,
+      composer: null,
+      params: {
+        bloomStrength: 1.5,
+        bloomThreshold: 0,
+        bloomRadius: 0
+      }
     }
   },
   mounted() {
@@ -27,6 +37,7 @@ export default {
     this.configScene()
     this.positionCamera()
     this.createObjects()
+    this.createPostProcess()
     this.bindWindowEvents()
     this.appendElement()
     this.startScene()
@@ -49,40 +60,63 @@ export default {
       const light = new THREE.AmbientLight(0xffffff, 1.0)
       this.scene.add(light)
       this.spotLight = new THREE.SpotLight(0xFFFFFF, 4, 100, Math.PI, 20, 0.5)
-      this.spotLight.position.set(0, 0, 10)
+      this.spotLight.position.set(0, 0, 30)
       this.scene.add(this.spotLight)
     },
     positionCamera() {
-      this.camera.position.set(0, 20, 35)
+      this.camera.position.set(0, 0, 60)
       this.camera.lookAt(this.origin)
     },
     // 処理
     createObjects() {
       this.stdMtr = new THREE.MeshStandardMaterial({ color: 'rgb(100, 10, 10)', roughness: 0, metalness: 0 })
+      this.obj = new THREE.Object3D()
+      this.scene.add(this.obj)
+      for (let i = 0; i < 30; i++) {
+        const geoBox = new THREE.BoxBufferGeometry(Math.PI, Math.sqrt(2), Math.E)
+        this.mshBox = new THREE.Mesh(geoBox, this.stdMtr)
+        this.mshBox.position.x = this.objectX
+        this.mshBox.position.y = this.objectY
+        this.mshBox.rotation.set(0, Math.PI / 2.0, 0)
+        this.mshBox.rotation.y = 4
+        this.mshBox.rotation.z = 12
+        this.mshBox.castShadow = true
+        this.mshBox.receiveShadow = true
+        this.obj.add(this.mshBox)
 
-      const geoBox = new THREE.BoxBufferGeometry(Math.PI, Math.sqrt(2), Math.E)
-      this.mshBox = new THREE.Mesh(geoBox, this.stdMtr)
-      this.mshBox.position.set(0, 0, 0)
-      this.mshBox.rotation.set(0, Math.PI / 2.0, 0)
-      this.mshBox.rotation.y = 4
-      this.mshBox.rotation.z = 20
-      this.mshBox.castShadow = true
-      this.mshBox.receiveShadow = true
-      this.scene.add(this.mshBox)
+        const geoSphere = new THREE.SphereBufferGeometry(1.5, 32, 32)
+        this.mshSphere = new THREE.Mesh(geoSphere, this.stdMtr)
+        this.mshSphere.position.x = this.objectX - 5
+        this.mshSphere.position.y = this.objectY
+        this.mshSphere.castShadow = true
+        this.mshSphere.receiveShadow = true
+        this.obj.add(this.mshSphere)
 
-      const geoSphere = new THREE.SphereBufferGeometry(1.5, 32, 32)
-      this.mshSphere = new THREE.Mesh(geoSphere, this.stdMtr)
-      this.mshSphere.position.set(-5, 0, 0)
-      this.mshSphere.castShadow = true
-      this.mshSphere.receiveShadow = true
-      this.scene.add(this.mshSphere)
+        const geoKnot = new THREE.TorusKnotBufferGeometry(1.5, 0.5, 100, 16)
+        this.mshKnot = new THREE.Mesh(geoKnot, this.stdMtr)
+        this.mshKnot.position.x = this.objectX + 5
+        this.mshKnot.position.y = this.objectY
+        this.mshKnot.castShadow = true
+        this.mshKnot.receiveShadow = true
+        this.obj.add(this.mshKnot)
 
-      const geoKnot = new THREE.TorusKnotBufferGeometry(1.5, 0.5, 100, 16)
-      this.mshKnot = new THREE.Mesh(geoKnot, this.stdMtr)
-      this.mshKnot.position.set(5, 0, 0)
-      this.mshKnot.castShadow = true
-      this.mshKnot.receiveShadow = true
-      this.scene.add(this.mshKnot)
+        if (this.objectX <= 20) this.objectX += 15
+        else {
+          this.objectX = -30
+          this.objectY += 8
+        }
+      }
+    },
+    createPostProcess() {
+      this.composer = new EffectComposer(this.renderer)
+      const renderPass = new RenderPass(this.scene, this.camera)
+      this.composer.addPass(renderPass)
+
+      const bloomPass = new UnrealBloomPass()
+      bloomPass.threshold = this.params.bloomThreshold
+      bloomPass.strength = this.params.bloomStrength
+      bloomPass.radius = this.params.bloomRadius
+      this.composer.addPass(bloomPass)
     },
     bindWindowEvents() {
       window.addEventListener('resize', this.handleWindowResize, false)
@@ -103,15 +137,15 @@ export default {
       this.renderer.render(this.scene, this.camera)
       this.camera.updateProjectionMatrix()
       this.renderer.setSize(window.innerWidth, window.innerHeight)
+      // this.composer.render(this.scene, this.camera)
+      this.composer.setSize(window.innerWidth, window.innerHeight)
 
       this.lightAngle -= 0.04
       this.spotLight.position.x = 8 * Math.sin(this.lightAngle)
       this.spotLight.position.y = 8 * Math.cos(this.lightAngle)
 
       this.payload -= 0.03
-      this.mshSphere.position.y = 0.4 * Math.sin(this.payload)
-      if (this.payload < -0.06) this.mshBox.position.y = 0.01 * Math.sin(this.payload)
-      if (this.payload < -0.08) this.mshKnot.position.y = 0.24 * Math.sin(this.payload)
+      this.obj.position.y = 0.4 * Math.sin(this.payload)
     }
   }
 }
